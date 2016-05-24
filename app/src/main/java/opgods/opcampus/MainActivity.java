7f24b1cea8cbfd409c645591e33c73ac59dd88df
Daniel Uroz <uzetta27@hotmail.com>
2016-05-24 23:06:31 +0200

    Refactorizado

--- app/src/main/java/opgods/opcampus/MainActivity.java
+++ app/src/main/java/opgods/opcampus/MainActivity.java
@@ -12,47 +12,29 @@ import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-import com.github.clans.fab.FloatingActionButton;
-import com.github.clans.fab.FloatingActionMenu;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.TileOverlayOptions;
-import com.google.android.gms.maps.model.TileProvider;
 
-import opgods.opcampus.maps.TileProviderFactory;
-import opgods.opcampus.parking.GetSlotsAdapter;
-import opgods.opcampus.parking.SlotsMarkerManager;
-import opgods.opcampus.teachers.GetTeachersAdapter;
-import opgods.opcampus.teachers.TeacherInfoWindow;
 import opgods.opcampus.teachers.TeacherMarkerManager;
 import opgods.opcampus.teachers.TeacherSearcher;
-import opgods.opcampus.util.Constants;
 
+
+/**
+ * Vista principal de la aplicación que contiene las secciones y el mapa
+ */
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private GoogleMap mMap;
     private MapView mapView;
     private MenuItem searchItem;
-    private SectionManager sectionManager;
+    private SectionViewManager sectionManager;
 
-    private FloatingActionMenu faMenu;
-    private FloatingActionButton fabPlanta_0;
-    private FloatingActionButton fabPlanta_1;
-    private FloatingActionButton fabPlanta_2;
-    private FloatingActionButton fabPlanta_3;
-    private FloatingActionButton fabPlanta_4;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
     private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,85 +50,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 mMap = googleMap;
-                sectionManager = new SectionManager(MainActivity.this, mMap);
-                sectionManager.initView();
+                sectionManager = new SectionViewManager(MainActivity.this, mMap);
             }
         });
-
-        findViewsById();
-        setButtonActions();
-
-        faMenu.setClosedOnTouchOutside(true);
-        faMenu.hideMenu(false);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        assert drawer != null;
-        drawer.setDrawerListener(toggle);
+        if (drawer != null) {
+            drawer.setDrawerListener(toggle);
+        }
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        assert navigationView != null;
-        navigationView.setNavigationItemSelectedListener(this);
-        if (savedInstanceState == null) {
-            navigationView.getMenu().performIdentifierAction(R.id.drawer_layout, 0);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+            if (savedInstanceState == null) {
+                navigationView.getMenu().performIdentifierAction(R.id.drawer_layout, 0);
+            }
         }
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void findViewsById() {
-        faMenu = (FloatingActionMenu) findViewById(R.id.fa_menu);
-        fabPlanta_0 = (FloatingActionButton) findViewById(R.id.planta_0);
-        fabPlanta_1 = (FloatingActionButton) findViewById(R.id.planta_1);
-        fabPlanta_2 = (FloatingActionButton) findViewById(R.id.planta_2);
-        fabPlanta_3 = (FloatingActionButton) findViewById(R.id.planta_3);
-        fabPlanta_4 = (FloatingActionButton) findViewById(R.id.planta_4);
-    }
-
-    private void setButtonActions() {
-        fabPlanta_0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setFloor("Planta 0", Constants.PLANTA_0, Constants.PROFESORES_P0);
-            }
-        });
-        fabPlanta_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setFloor("Planta 1", Constants.PLANTA_1, Constants.PROFESORES_P1);
-            }
-        });
-        fabPlanta_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setFloor("Planta 2", Constants.PLANTA_2, Constants.PROFESORES_P2);
-            }
-        });
-        fabPlanta_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setFloor("Planta 3", Constants.PLANTA_3, Constants.PROFESORES_P3);
-            }
-        });
-        fabPlanta_4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setFloor("Planta 4", Constants.PLANTA_4, Constants.PROFESORES_P4);
-            }
-        });
-    }
-
-    private void setFloor(String title, String planta, String profesores) {
-        setTitle(title);
-        mMap.clear();
-        TileProvider tileProvider = TileProviderFactory.getTileProvider(planta);
-        mMap.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
-        faMenu.close(true);
-        new GetTeachersAdapter(TeacherMarkerManager.getInstance(MainActivity.this, mMap)).execute(profesores);
-    }
 
     @Override
     public void onBackPressed() {
@@ -162,7 +88,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
         searchView.setQueryHint("Buscar profesores...");
@@ -172,23 +97,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         searchView.setOnSuggestionListener(searcher);
         searchItem = menu.findItem(R.id.action_search);
         searchItem.setVisible(false);
+        sectionManager.initView(searchItem);
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_search) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_search || super.onOptionsItemSelected(item);
     }
+
 
     @Override
     protected void onDestroy() {
@@ -196,11 +116,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mapView.onDestroy();
     }
 
+
     @Override
     protected void onResume() {
         super.onResume();
         mapView.onResume();
     }
+
 
     @Override
     protected void onPause() {
@@ -208,11 +130,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mapView.onPause();
     }
 
+
     @Override
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
     }
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -220,45 +144,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mapView.onSaveInstanceState(outState);
     }
 
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_parking) {
-            searchItem.setVisible(false);
-            mMap.clear();
-            setTitle(R.string.parking);
-            int zoomLevel = 16;
-            faMenu.hideMenu(false);
-
-            LatLng adaByron = new LatLng(41.683662, -0.887611);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(adaByron, zoomLevel));
-            TileProvider tileProvider = TileProviderFactory.getTileProvider(Constants.PLAZAS);
-            mMap.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
-            new GetSlotsAdapter(SlotsMarkerManager.getInstance(mMap, MainActivity.this)).execute();
+            sectionManager.parkingView(searchItem);
         } else if (id == R.id.nav_teacher) {
-            searchItem.setVisible(true);
-            mMap.clear();
-            setTitle("Planta 0");
-            int zoomLevel = 19;
-            faMenu.showMenu(false);
-
-            LatLng adaByron = new LatLng(41.683982, -0.888867);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(adaByron, zoomLevel));
-            TileProvider tileProvider = TileProviderFactory.getTileProvider(Constants.PLANTA_0);
-            mMap.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
-            mMap.setInfoWindowAdapter(new TeacherInfoWindow(getApplicationContext()));
-            new GetTeachersAdapter(TeacherMarkerManager.getInstance(MainActivity.this, mMap)).execute(Constants.PROFESORES_P0);
+            sectionManager.teacherView(searchItem);
         } else if (id == R.id.nav_cafe) {
-            searchItem.setVisible(false);
-            mMap.clear();
-            setTitle(R.string.cafe);
-            int zoomLevel = 20;
-            faMenu.hideMenu(false);
-
-            LatLng adaByron = new LatLng(41.683646, -0.888620);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(adaByron, zoomLevel));
+            sectionManager.cafeView(searchItem);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -268,43 +164,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
-
     @Override
     public void onStart() {
         super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
         client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://opgods.opcampus/http/host/path")
-        );
+        Action viewAction = Action.newAction(Action.TYPE_VIEW, "Main Page", Uri.parse("http://host/path"),
+                Uri.parse("android-app://opgods.opcampus/http/host/path"));
         AppIndex.AppIndexApi.start(client, viewAction);
     }
+
 
     @Override
     public void onStop() {
         super.onStop();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://opgods.opcampus/http/host/path")
-        );
+        Action viewAction = Action.newAction(Action.TYPE_VIEW, "Main Page", Uri.parse("http://host/path"),
+                Uri.parse("android-app://opgods.opcampus/http/host/path"));
         AppIndex.AppIndexApi.end(client, viewAction);
         client.disconnect();
     }
---
+++ app/src/main/java/opgods/opcampus/SectionViewManager.java
@@ -0,0 +1,181 @@
+package opgods.opcampus;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.maps.model.TileProvider;
+
+import opgods.opcampus.maps.TileProviderFactory;
+import opgods.opcampus.parking.GetSlotsAdapter;
+import opgods.opcampus.parking.SlotInfoWindow;
+import opgods.opcampus.parking.SlotsMarkerManager;
+import opgods.opcampus.teachers.GetTeachersAdapter;
+import opgods.opcampus.teachers.TeacherInfoWindow;
+import opgods.opcampus.teachers.TeacherMarkerManager;
+import opgods.opcampus.util.Constants;
+
+/**
+ * Clase encargada de mostrar la vista de la aplicación según las acciones del usuario
+ */
+public class SectionViewManager {
+    private MainActivity mainActivity;
+    private GoogleMap map;
+
+    private FloatingActionMenu faMenu;
+    private FloatingActionButton fabPlanta_0;
+    private FloatingActionButton fabPlanta_1;
+    private FloatingActionButton fabPlanta_2;
+    private FloatingActionButton fabPlanta_3;
+    private FloatingActionButton fabPlanta_4;
+
+
+    public SectionViewManager(MainActivity mainActivity, GoogleMap map) {
+        this.mainActivity = mainActivity;
+        this.map = map;
+        findViewsById();
+        setButtonActions();
+    }
+
+
+    /**
+     * Busca todos los elementos de la vista por su ID
+     */
+    private void findViewsById() {
+        faMenu = (FloatingActionMenu) mainActivity.findViewById(R.id.fa_menu);
+        fabPlanta_0 = (FloatingActionButton) mainActivity.findViewById(R.id.planta_0);
+        fabPlanta_1 = (FloatingActionButton) mainActivity.findViewById(R.id.planta_1);
+        fabPlanta_2 = (FloatingActionButton) mainActivity.findViewById(R.id.planta_2);
+        fabPlanta_3 = (FloatingActionButton) mainActivity.findViewById(R.id.planta_3);
+        fabPlanta_4 = (FloatingActionButton) mainActivity.findViewById(R.id.planta_4);
+    }
+
+
+    /**
+     * Inicializa la vista
+     */
+    public void initView(MenuItem searchItem) {
+        faMenu.setClosedOnTouchOutside(true);
+        faMenu.hideMenu(false);
+        map.getUiSettings().setMapToolbarEnabled(false);
+        parkingView(searchItem);
+    }
+
+
+    /**
+     * Muestra la configuración de la sección de parking
+     *
+     * @param searchItem item del menú para ocultarlo
+     */
+    public void parkingView(MenuItem searchItem) {
+        new GetSlotsAdapter(SlotsMarkerManager.getInstance(map, mainActivity)).execute();
+        map.setInfoWindowAdapter(new SlotInfoWindow(mainActivity.getApplicationContext()));
+        changeView(searchItem, false, mainActivity.getString(R.string.parking), 16, new LatLng(41.683662, -0.887611), Constants.PLAZAS);
+    }
+
+
+    /**
+     * Muestra la configuración de la sección de profesores
+     *
+     * @param searchItem item del menú para mostrarlo
+     */
+    public void teacherView(MenuItem searchItem) {
+        new GetTeachersAdapter(TeacherMarkerManager.getInstance(mainActivity, map)).execute(Constants.PROFESORES_P0);
+        map.setInfoWindowAdapter(new TeacherInfoWindow(mainActivity.getApplicationContext()));
+        changeView(searchItem, true, "Planta 0", 19, new LatLng(41.683982, -0.888867), Constants.PLANTA_0);
+        faMenu.showMenu(false);
+    }
+
+
+    /**
+     * Muestra la configuración de la sección de cafetería
+     *
+     * @param searchItem item del menú para ocultarlo
+     */
+    public void cafeView(MenuItem searchItem) {
+        changeView(searchItem, false, mainActivity.getString(R.string.cafe), 20, new LatLng(41.683646, -0.888620), "");
+        map.clear();
+    }
+
+
+    /**
+     * Cambia los elementos de la vista
+     *
+     * @param searchItem item del menú
+     * @param searchVisibility ocultar/mostrar searchItem
+     * @param title título del activity
+     * @param zoom nivel de zoom del mapa
+     * @param place lugar a mostrar en el mapa
+     * @param layer capa a pedir al Geoserver
+     */
+    private void changeView(MenuItem searchItem, boolean searchVisibility, String title, int zoom,
+                            LatLng place, String layer) {
+        searchItem.setVisible(searchVisibility);
+        map.clear();
+        mainActivity.setTitle(title);
+        faMenu.hideMenu(false);
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(place, zoom));
+        TileProvider tileProvider = TileProviderFactory.getTileProvider(layer);
+        map.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
+    }
+
+
+    /**
+     * Establece las acciones de los botones de plantas
+     */
+    private void setButtonActions() {
+        fabPlanta_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFloor("Planta 0", Constants.PLANTA_0, Constants.PROFESORES_P0);
+            }
+        });
+        fabPlanta_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFloor("Planta 1", Constants.PLANTA_1, Constants.PROFESORES_P1);
+            }
+        });
+        fabPlanta_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFloor("Planta 2", Constants.PLANTA_2, Constants.PROFESORES_P2);
+            }
+        });
+        fabPlanta_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFloor("Planta 3", Constants.PLANTA_3, Constants.PROFESORES_P3);
+            }
+        });
+        fabPlanta_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFloor("Planta 4", Constants.PLANTA_4, Constants.PROFESORES_P4);
+            }
+        });
+    }
+
+
+    /**
+     * Muestra una determinada planta y carga los profesores que hay en ella
+     *
+     * @param title título de la actividad
+     * @param planta planta a mostrar
+     * @param profesores profesores a mostrar
+     */
+    private void setFloor(String title, String planta, String profesores) {
+        mainActivity.setTitle(title);
+        map.clear();
+        TileProvider tileProvider = TileProviderFactory.getTileProvider(planta);
+        map.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
+        faMenu.close(true);
+        new GetTeachersAdapter(TeacherMarkerManager.getInstance(mainActivity, map)).execute(profesores);
+    }
+}
